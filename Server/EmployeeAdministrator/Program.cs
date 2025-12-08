0d13bb04e02121fb088ba1a081b1e5a952e9b373
LorencBzhetaj <bzhetajlorenc@gmail.com>
2025-12-08 07:41:20 -0800

    Database Creation and Initial Folder Structure
    Created Database With user / roles. Seeded 1 Admin Account

--- Server/EmployeeAdministrator/Program.cs
+++ Server/EmployeeAdministrator/Program.cs
@@ -1,10 +1,28 @@
+using EmployeeAdministrator.DataLayer;
+using EmployeeAdministrator.Helpers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    await SeedData.Initialize(services);
+}
 
 app.UseSwagger();
 app.UseSwaggerUI(c =>
@@ -14,7 +32,7 @@ app.UseSwaggerUI(c =>
 });
 
 app.UseHttpsRedirection();
+app.UseAuthentication(); 
 app.UseAuthorization();
 app.MapControllers();
-
 app.Run();
